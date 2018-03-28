46ee3776416a44b9c26c66b08df25df0f7881d36
JannikMaes <jannik.maes@gmail.com>
2018-03-28 14:10:54 +0200

    Acitivity Main angepasst

--- app/src/main/java/com/example/mfwis415a/food4life/MainActivity.java
+++ app/src/main/java/com/example/mfwis415a/food4life/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         dataSource = new TagebuchDataSource(this);
 
-        addMeal = (Button) findViewById(R.id.Hinzufuegen);
+       /* addMeal = (Button) findViewById(R.id.Hinzufuegen);
         foodList = (Button) findViewById(R.id.goToFoodList);
 
         addMeal.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 myIntent.putExtra("key", "test"); //Optional parameters
                 MainActivity.this.startActivity(myIntent);
             }
-        });
+        })*/
 
         dataSource.open();
     }
