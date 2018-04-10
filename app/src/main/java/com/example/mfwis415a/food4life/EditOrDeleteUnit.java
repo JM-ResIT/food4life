7331a62238135c7dbe609499b9c65be5e5e6cdf5
HenrykSchaffrath <henryk.scha22@gmail.com>
2018-04-10 18:40:31 +0200

    Unit izz da

--- app/src/main/java/com/example/mfwis415a/food4life/EditOrDeleteUnit.java
+++ app/src/main/java/com/example/mfwis415a/food4life/EditOrDeleteUnit.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -14,12 +16,41 @@ public class EditOrDeleteUnit extends AppCompatActivity {
     private TagebuchDataSource dataSource;
     private EditText UnitName;
 
-    private static final String LOG_TAG = EditOrDeleteFood.class.getSimpleName();
+    private static final String LOG_TAG = EditOrDeleteUnit.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_or_delete_unit);
+
+        Button deleteUnit = (Button) findViewById(R.id.deleteUnit;
+        Button editUnit = (Button) findViewById(R.id.editUnit);
+
+        dataSource = new TagebuchDataSource(this);
+
+        Intent mIntent = getIntent();
+        int position = mIntent.getIntExtra("position", 0);
+
+        dataSource.open();
+        lm_id = dataSource.getRealIdFromLM(position);
+
+        loadUnitData();
+
+        deleteUnit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataSource.updateStatusOfLM(lm_id, 0);
+                Intent myIntent = new Intent(EditOrDeleteUnit.this, UnitList.class);
+                EditOrDeleteUnit.this.startActivity(myIntent);
+            }
+        });
+
+        editUnit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editUnit();
+            }
+        });
     }
 
     private void goBack() {
